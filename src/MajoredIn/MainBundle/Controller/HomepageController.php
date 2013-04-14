49db6e53544b1cd3f67ac7b852f95aee04095589
NickMeves <nicholas.meves@gmail.com>
2013-04-13 17:21:35 -0700

    Streamline canonical urls

--- src/MajoredIn/MainBundle/Controller/HomepageController.php
+++ src/MajoredIn/MainBundle/Controller/HomepageController.php
@@ -8,6 +8,13 @@ class HomepageController extends Controller
 {
     public function indexAction()
     {
+        //redirect away from weird facebook link query string for homepage
+        $queryString = $this->get('request')->query->all();
+        if (isset($queryString['fb_action_ids'])) {
+            $response = $this->redirect($this->generateUrl('mi_main_homepage', array(), true), 301);
+            return $response;
+        }
+        
         if (in_array($this->get('kernel')->getEnvironment(), array('mobile', 'mobile_dev'))) {
             $response = $this->render('MajoredInMainBundle:Homepage:homepage.mobile.twig');
         }
